28662b16245cd2cc11b75411b03d3563666bf08a
BelMabrouk-Bacem <78115937+BelMabrouk-Bacem@users.noreply.github.com>
2023-08-29 08:54:57 +0100

    Add files via upload

---
+++ buzzeur.ino
@@ -0,0 +1,43 @@
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+//pin 12 has a button
+int button= 12;
+
+void setup() {
+  // put your setup code here, to run once:
+ pinMode (button, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ int buttonState = digitalRead(button);
+
+  if (buttonState == 1){
+     // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(8);
+  }
+ }
+}
